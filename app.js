25efd7ccbab1cb5f913cfc3b38588934a8cab811
Tomi Saputra <74924041+TomiSaputraa@users.noreply.github.com>
2023-09-01 06:14:08 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const shareButtons = document.querySelectorAll(".tile-share-button");
+console.log(shareButtons);
+
+async function copyText(e) {
+  // prevent button going to site
+  e.preventDefault();
+  const link = this.getAttribute("link");
+  console.log(link);
+
+  try {
+    await navigator.clipboard.writeText(link);
+    alert("Copied text link : " + link);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+shareButtons.forEach((shareButton) =>
+  shareButton.addEventListener("click", copyText)
+);
