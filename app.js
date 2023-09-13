0fbbf95cdeea91f3260455006af38d8178bdb2a4
Tomi Saputra <tommycodingjj@gmail.com>
2023-09-13 13:03:50 +0700

    Update all link tree

--- app.js
+++ app.js
@@ -1,20 +1,141 @@
-const shareButtons = document.querySelectorAll(".tile-share-button");
-console.log(shareButtons);
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
 
-async function copyText(e) {
-  // prevent button going to site
-  e.preventDefault();
-  const link = this.getAttribute("link");
-  console.log(link);
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
 
-  try {
-    await navigator.clipboard.writeText(link);
-    alert("Copied text link : " + link);
-  } catch (error) {
-    console.log(error);
+/* Otherwise just put the config content (json): */
+
+particlesJS(
+  "particles-js",
+
+  {
+    retinaDetect: true,
+    fpsLimit: 90,
+    fullScreen: {
+      enable: false,
+    },
+    particles: {
+      number: {
+        value: 100,
+        density: {
+          enable: true,
+          area: 800,
+        },
+      },
+      color: {
+        value: ["c912ed", "00bfff", "22dd22", "ffd500", "ff8000", "ff2600"],
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "000",
+        },
+        polygon: {
+          sides: 5,
+        },
+        image: {
+          src: "img/github.svg",
+          width: 100,
+          height: 100,
+        },
+      },
+      opacity: {
+        value: 0.9,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          minimumValue: 0.5,
+          sync: false,
+        },
+      },
+      size: {
+        value: 4,
+        random: {
+          enable: true,
+          minimumValue: 2,
+        },
+        anim: {
+          enable: false,
+          speed: 30,
+          minimumValue: 0.1,
+          sync: true,
+        },
+      },
+      links: {
+        enable: true,
+        distance: 75,
+        color: "999",
+        opacity: 0.9,
+        width: 1,
+        consent: false,
+        blink: false,
+      },
+      move: {
+        enable: true,
+        speed: 2,
+        direction: "none",
+        random: false,
+        straight: false,
+        outMode: "bounce",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200,
+        },
+      },
+    },
+    interactivity: {
+      detectsOn: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse",
+        },
+        onclick: {
+          enable: true,
+          mode: "push",
+        },
+        resize: true,
+      },
+      modes: {
+        attract: {
+          distance: 100,
+          duration: 1,
+          speed: 5,
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+        },
+        grab: {
+          distance: 400,
+          links: {
+            opacity: 1,
+          },
+        },
+        push: {
+          quantity: 3,
+        },
+        remove: {
+          quantity: 2,
+        },
+        repulse: {
+          distance: 100,
+          duration: 1,
+        },
+      },
+    },
+    key: "3m@62^K^88745%",
   }
-}
-
-shareButtons.forEach((shareButton) =>
-  shareButton.addEventListener("click", copyText)
 );
